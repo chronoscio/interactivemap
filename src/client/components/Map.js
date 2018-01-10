053be2c0a4bba561a6e25d02e9efb51b313ab13c
Ata Ali Kilicli <ataalikilicli@gmail.com>
2018-01-10 19:47:52 +0300

    Added geolayer rendering

--- src/client/components/Map.js
+++ src/client/components/Map.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet';
 
-const position = [51.505, -0.09]
+const position = [30, 20]
 
 class LeafletMap extends React.Component {
 
     render() {
         // this.props.demo.geo
         // this.props.demo.attrs
+        var geoLayer;
+        if(this.props.demo.geo !== undefined && this.props.demo.geo !== null)
+        {
+          //console.log(this.props.demo.geo.data.geojson);
+          geoLayer = <GeoJSON data={this.props.demo.geo.data.geojson} />
+        }
         return (
-            <Map center={position} zoom={13}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Marker position={position}>
-                    <Popup>
-                        <span>A pretty CSS3 popup.<br />Easily customizable.</span>
-                    </Popup>
-                </Marker>
+            <Map center={position} zoom={4}>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              />
+              {geoLayer}
             </Map>
         );
     }
